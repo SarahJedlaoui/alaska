e460dbb98d1a0a6ad67d5c03ff9402dc96e3b635
maherkhelifi88 <admin@bei.dev>
2023-12-14 01:52:13 -0800

    updates

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost"],
+    domains: ["localhost","tiny-druid-926a04.netlify.app","*",""],
     remotePatterns: [
       {
         protocol: "https",
