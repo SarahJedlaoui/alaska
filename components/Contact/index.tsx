3a40ccd2c1b5fab588986f428dedb7dc19874aa6
SarahJedlaoui <sarajedlaoui999@gmail.com>
2023-12-28 17:18:35 +0100

    commit

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -12,7 +12,7 @@ const Contact = () => {
               "
             >
               <h2 className="mb-3 text-2xl font-bold text-black dark:text-white sm:text-3xl lg:text-2xl xl:text-3xl">
-                Need Help? Open a Ticket
+                Need Help? Contact Us
               </h2>
               <p className="mb-12 text-base font-medium text-body-color">
                 Our support team will get back to you ASAP via email.
--- components/Header/ThemeToggler.tsx
+++ components/Header/ThemeToggler.tsx
@@ -4,7 +4,7 @@ const ThemeToggler = () => {
   const { theme, setTheme } = useTheme();
   return (
     <button aria-label='theme toggler'
-      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
       className="flex items-center justify-center text-black rounded-full cursor-pointer bg-gray-2 dark:bg-dark-bg h-9 w-9 dark:text-white md:h-14 md:w-14"
     >
       <svg
